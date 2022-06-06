aee5dc4aae3942b56e222837f38fcdda2402c3ff
daltonmitchell <dzack34@gmail.com>
2022-06-06 14:37:27 -0700

    adds code to remove  for each spin and update balance

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ const slot = document.getElementById('slot')
 const slot2 = document.getElementById('slot2') 
 const slot3 = document.getElementById('slot3') 
 
-let balance = 20000;
+let balance = 10000;
+
+const bank = document.getElementById('bank');
+bank.innerText = 'Balance: $' + balance;
+
 
 
 let tiles = ['j', 'q', 'k', 'a', 'c']
@@ -19,6 +23,9 @@ document.getElementById('button').addEventListener('click', onSpin);
 
 /*---------------functions-----------------*/
 function onSpin(){
+    balance = balance - 100;
+    bank.innerText = 'Balance: $' + balance;
+
     /* chooses a tile to place in each slot */
     slot.innerText = tiles[Math.floor(Math.random() * 5)];
     slot2.innerText = tiles[Math.floor(Math.random() * 5)];
