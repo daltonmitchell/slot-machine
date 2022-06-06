8d40a5373f1e212a33e48347fd4d995a46816f7d
daltonmitchell <dzack34@gmail.com>
2022-06-06 13:11:53 -0700

    reformats for 3 cells total and addsconst variables

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 /*----------------const---------------*/
-const cell = document.getElementById('cell')
-const cell1 = document.getElementById('cell1')
-const cell2 = document.getElementById('cell2')
-const cell3 = document.getElementById('cell3')
-const cell4 = document.getElementById('cell4')
-const cell5 = document.getElementById('cell5')
-const cell6 = document.getElementById('cell6')
-const cell7 = document.getElementById('cell7')
-const cell8 = document.getElementById('cell8')
+const slot = document.getElementById('slot') 
+const slot2 = document.getElementById('slot2') 
+const slot3 = document.getElementById('slot3') 
+
+let balance = 100000;
+
+let tile = 'j';
+let tile2 = 'q';
+let tile3 = 'k';
+let tile4 = 'a';
+let tile5 = 'c';
+
+
+
+/*-------------event listeners-------------*/
+document.getElementById('button').addEventListener('click', onSpin)
+
+
+
+
+
+/*---------------functions-----------------*/
+function onSpin(){
+    
+}
