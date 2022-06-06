f1a3e52747521aacd734a9e5d0bf7abe5b341bb1
daltonmitchell <dzack34@gmail.com>
2022-06-06 14:56:13 -0700

    adds code to add winnings to earnings

--- js/main.js
+++ js/main.js
@@ -24,12 +24,25 @@ document.getElementById('button').addEventListener('click', onSpin);
 /*---------------functions-----------------*/
 function onSpin(){
     balance = balance - 100;
-    bank.innerText = 'Balance: $' + balance;
-
+    
     /* chooses a tile to place in each slot */
     slot.innerText = tiles[Math.floor(Math.random() * 5)];
     slot2.innerText = tiles[Math.floor(Math.random() * 5)];
     slot3.innerText = tiles[Math.floor(Math.random() * 5)];
+    
+    /* adds winnings to player balance */
+    const result = slot.innerText + slot2.innerText + slot3.innerText;
+    if(result === 'ccc'){
+        balance = balance + 50000;
+    }else if(result === 'aaa'){
+        balance = balance + 10000;
+    }else if(result === 'kkk'){
+        balance = balance + 5000;
+    }else if(result === 'qqq'){
+        balance = balance + 2000;
+    }else if(result === 'jjj'){
+        balance = balance + 1000;
+    }
 
-
+    bank.innerText = 'Balance: $' + balance;
 }
